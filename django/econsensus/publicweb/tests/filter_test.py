d414380ddf6aec2d390656a3749e96c8f99b6e4c
Philip McMahon <13phil13@gmail.com>
2013-05-17 18:16:45 +0200

    Test for no user details added.

--- django/econsensus/publicweb/tests/filter_test.py
+++ django/econsensus/publicweb/tests/filter_test.py
@@ -5,11 +5,12 @@ from django.contrib.comments.models import Comment
 
 # test custom template filters
 class FilterTest(EconsensusTestCase):
-    
+ 
     def test_get_user_name_from_comment(self):
         c = Comment()
         c.user = self.betty
         self.assertEqual(publicweb_filters.get_user_name_from_comment(c), "betty")
-        c = Comment()
+        c.user=None
+        self.assertEqual(publicweb_filters.get_user_name_from_comment(c), "An Anonymous Contributor")
         c.user_name = "Harry"
         self.assertEqual(publicweb_filters.get_user_name_from_comment(c), "Harry")
